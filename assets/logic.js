d799f978dccf5e176a032e012cea02f4f30582e6
GreenCord <steve@nevizen.com>
2017-12-09 10:44:55 -0500

    Tweak FirebaseUI for auth part deux

--- assets/logic.js
+++ assets/logic.js
@@ -63,23 +63,62 @@ $('#login-submit').on('click', function(e){
   $('#emailmodal').modal('close');
 });
 // Handle authenticated users
-firebase.auth().onAuthStateChanged(function(user){
-  if (user) {
-    // User is authenticated
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    $('#email-login').hide();
-    $('#ttr-login').hide();
-    $('#ttr-login-popup').hide();
-    $('#authuser').html('<i class="material-icons">person</i> Welcome, '+ email);
-    $('#authlogout').css('display','inline-block');
-  }
-});
+// firebase.auth().onAuthStateChanged(function(user){
+//   if (user) {
+//     // User is authenticated
+//     var displayName = user.displayName;
+//     var email = user.email;
+//     var emailVerified = user.emailVerified;
+//     var photoURL = user.photoURL;
+//     var isAnonymous = user.isAnonymous;
+//     var uid = user.uid;
+//     var providerData = user.providerData;
+//     $('#email-login').hide();
+//     $('#ttr-login').hide();
+//     $('#ttr-login-popup').hide();
+//     $('#authuser').html('<i class="material-icons">person</i> Welcome, '+ email);
+//     $('#authlogout').css('display','inline-block');
+//   }
+// });
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var uid = user.uid;
+        var phoneNumber = user.phoneNumber;
+        var providerData = user.providerData;
+        user.getIdToken().then(function(accessToken) {
+          document.getElementById('sign-in-status').textContent = 'Signed in';
+          document.getElementById('sign-in').textContent = 'Sign out';
+          document.getElementById('account-details').textContent = JSON.stringify({
+            displayName: displayName,
+            email: email,
+            emailVerified: emailVerified,
+            phoneNumber: phoneNumber,
+            photoURL: photoURL,
+            uid: uid,
+            accessToken: accessToken,
+            providerData: providerData
+          }, null, '  ');
+        });
+      } else {
+        // User is signed out.
+        document.getElementById('sign-in-status').textContent = 'Signed out';
+        document.getElementById('sign-in').textContent = 'Sign in';
+        document.getElementById('account-details').textContent = 'null';
+      }
+    }, function(error) {
+      console.log(error);
+    });
+  };
+
+  window.addEventListener('load', function() {
+    initApp()
+  });
 // Email Logout
 $('#authlogout').on('click',function(e){
   e.preventDefault();
